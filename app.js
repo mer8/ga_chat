6622beb638e3453429a45facdafbacd85fd7ee9a
alexander <ag.saldivar@gmail.com>
2014-10-21 22:34:49 -0400

    create an app.js and require express and http so you can define your application with the express framework and create your own server

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var app = require('express');
+/////////// Foundation of Application ///////////
+
+// require the dependencies you want to use
+var express = require('express'), // require express so that we can utilize it as framework
+    http = require('http'); // we will use this node module to create a server
+
+var app = express(); // our app is defined as the express module
+
+http.createServer(app).listen(3000); // create your first node server!
